097cb67172b1199c3214f20c670004c0f92bf9b4
Om Rindhe <153447095+omrindhe105@users.noreply.github.com>
2025-08-30 22:01:13 +0530

    Update Index.tsx

--- src/app/pages/Index.tsx
+++ src/app/pages/Index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import HeroSection from "../components/ui/HeroSection";
 import Navbar from "../components/ui/Navbar";
 import AboutSection from "../components/ui/AboutSection";
-import WorkSection from "../components/ui/WorkSection";
 import SkillsSection from "../components/ui/SkillsSection";
 import ContactSection from "../components/ui/ContactSection";
 import Footer from "../components/ui/Footer";
@@ -14,7 +13,6 @@ const Index = () => {
       <main>
         <HeroSection />
         <AboutSection />
-{/*         <WorkSection /> */}
         <SkillsSection />
         <ContactSection />
       </main>
